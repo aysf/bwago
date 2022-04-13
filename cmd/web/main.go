ccde5e35da1e540f72a0cf218a4b9c64c15a1c09
aysf <ansufw@gmail.com>
2022-04-13 07:30:57 +0700

    converting html to go tmpl

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -48,7 +48,7 @@ func main() {
 	// http.HandleFunc("/", handlers.Home)
 	// http.HandleFunc("/about", handlers.About)
 
-	http.Handle("/src/", http.StripPrefix("/src", http.FileServer(http.Dir("./static"))))
+	// http.Handle("/static/*", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 
 	log.Println("application running on port 8080")
 	// http.ListenAndServe(":8080", nil)
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -64,3 +64,28 @@ func (m *Repository) About(rw http.ResponseWriter, r *http.Request) {
 		StringMap: stringMap,
 	})
 }
+
+// About handles about page
+func (m *Repository) Contact(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "contact.page.tmpl", &models.TemplateData{})
+}
+
+// Reservation renders the make a reservation page and display form
+func (m *Repository) Reservation(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "make-reservation.page.tmpl", &models.TemplateData{})
+}
+
+// Generals renders the room page
+func (m *Repository) Generals(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "generals.page.tmpl", &models.TemplateData{})
+}
+
+// Majors renders the room page
+func (m *Repository) Majors(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "majors.page.tmpl", &models.TemplateData{})
+}
+
+// Majors renders the room page
+func (m *Repository) Availability(rw http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(rw, "search-availability.page.tmpl", &models.TemplateData{})
+}

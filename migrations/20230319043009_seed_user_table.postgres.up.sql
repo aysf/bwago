35cc534b7a890409b31492d699be99c4ff71658c
aysf <ansufw@gmail.com>
2023-03-19 05:33:38 +0100

    update migration file

---
+++ migrations/20230319043009_seed_user_table.postgres.up.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    public.users (
+        email,
+        first_name,
+        last_name,
+        "password",
+        access_level,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'admin@here.com',
+        'ananto',
+        'wicaksono',
+        '$2a$12$MraqY9Th.iwdCDx.31ZAnOdafFOACF5chCfF1KI9hopz6aNHwrpXO',
+        3,
+        '2023-01-02 00:00:00.000',
+        '2023-01-02 00:00:00.000'
+    );

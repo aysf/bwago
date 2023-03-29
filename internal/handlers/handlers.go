2af79a60a1aa18907ba869c11331ead4cd755215
aysf <ansufw@gmail.com>
2023-03-29 19:06:38 +0200

    add calendar feature

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -790,12 +790,18 @@ func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *htt
 		}
 	}
 
+	//
 	for name, _ := range r.PostForm {
 		if strings.HasPrefix(name, "add_block") {
 			exploded := strings.Split(name, "_")
 			roomID, _ := strconv.Atoi(exploded[2])
+			t, _ := time.Parse("2006-01-2", exploded[3])
 			// insert new block
-			log.Println("would insert block for room id", roomID, "for date", exploded[3])
+			err := m.DB.InsertBlockForRoom(roomID, t)
+			if err != nil {
+				log.Println(err)
+			}
+			// log.Println("would insert block for room id", roomID, "for date", exploded[3])
 		}
 	}
 
